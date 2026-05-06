bd2ae25459d15a31beb985a80f2ab2a2acfeed0f
Shourya <adroit.shourya@gmail.com>
2026-05-05 20:54:39 -0700

    downgraded to lowever version of react and made props

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,9 +1,17 @@
-import Button from './components/Button.jsx';
+import Student from './components/Student.jsx';
 
 function App() {
-  return (<>
-      <Button />
-  </>);
+  return (
+    <>
+      <Student name="SpongeBob" age="nah" isStudent={true}/>
+      <Student name="Patrick" age={43} isStudent={false}/>
+      <Student name="Squidward" age={50} isStudent={false}/>
+      <Student name="Sandy" age={32} isStudent={true}/>
+
+      <Student /> 
+
+    </>
+  );
 }
 
 export default App
